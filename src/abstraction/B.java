b13d9daf332368e18cac45b242cadb64dda5fa5b
dharmender <dharmender@dharmendersingh>
2019-09-01 23:42:39 +0530

    first commit

---
+++ src/abstraction/B.java
@@ -0,0 +1,27 @@
+package abstraction;
+
+interface C{
+	
+	void test1();
+	static void test2() {
+		System.out.println("Hi..");
+	}
+	
+//	void test3(){
+//		System.out.println("hiii.."); cant develop non static methods inside interface.
+//	}
+//	No constructors in interface. Only static final methods are allowed.
+}
+public class B implements C {
+
+	public void test1() {
+		System.out.println("Hello..");
+	}
+	public static void main(String[] args) {
+
+		B vr = new B();
+		vr.test1();
+		C.test2();
+	}
+
+}
