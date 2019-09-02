d73bc10557cdbfdf1325a3249c079072b3253dcc
dharm.09@gmail.com <dharm.09@gmail.com>
2019-09-02 20:01:40 +0530

    reverse a String

---
+++ src/interviewProgramming/ReverseAString.java
@@ -0,0 +1,16 @@
+package interviewProgramming;
+
+public class ReverseAString {
+
+	public static void main(String[] args) {
+
+		String s = "I am the Programmer";
+		String rev = "";
+		for(int i =s.length()-1; i>=0; i--) {
+			rev = rev + s.charAt(i);
+			
+		}
+		System.out.println(rev);
+	}
+
+}
