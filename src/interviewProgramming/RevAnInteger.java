fa8877394e4da2b07f21b120f55c5383b2c56385
dharm.09@gmail.com <dharm.09@gmail.com>
2019-09-07 00:04:08 +0530

    reverse an integer

---
+++ src/interviewProgramming/RevAnInteger.java
@@ -0,0 +1,19 @@
+package interviewProgramming;
+
+public class RevAnInteger {
+
+	public static void main(String[] args) {
+
+		int a = 1243;
+		int rev =0;
+		while(a>0) {
+			int rem = a % 10;
+			
+			rev = (rev*10)+rem;
+			a = a/10;
+		}
+		
+		System.out.println(rev);
+	}
+
+}
